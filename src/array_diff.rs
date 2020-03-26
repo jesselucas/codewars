49e31b73179ff11d9cd37fc9123d93c688b13f3d
Jesse Lucas <jesse@jesselucas.com>
2020-03-26 10:45:46 -0400

    array diff kata

---
+++ src/array_diff.rs
@@ -0,0 +1,17 @@
+#[allow(dead_code)]
+fn array_diff<T: PartialEq + Copy>(a: Vec<T>, b: Vec<T>) -> Vec<T> {
+    return a.into_iter().filter(|n| !b.contains(n)).collect();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn returns_expected() {
+        assert_eq!(array_diff(vec![1, 2], vec![1]), vec![2]);
+        assert_eq!(array_diff(vec![1, 2, 2], vec![1]), vec![2, 2]);
+        assert_eq!(array_diff(vec![1, 2, 2], vec![2]), vec![1]);
+        assert_eq!(array_diff(vec![1, 2, 2], vec![]), vec![1, 2, 2]);
+        assert_eq!(array_diff(vec![], vec![1, 2]), vec![]);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
+mod array_diff;
 mod create_phone_number;
 mod reversed_strings;
