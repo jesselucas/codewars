e64d48d133ae7904d38f0c96cf7ba13eec0d7a84
Jesse Lucas <jesse@jesselucas.com>
2021-03-13 20:13:52 -0500

    write disemvowel function

--- c/disemvowel_trolls/main.c
+++ c/disemvowel_trolls/main.c
@@ -1,27 +1,50 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-//const char *vowels[] = {"a", "e", "i", "o", "u"};
+// const char *vowels[] = {"a", "e", "i", "o", "u"};
 const char *vowels = "aeiou";
 
-// solution must allocate all required memory
+// Solution must allocate all required memory
 // and return a free-able buffer to the caller.
 char *
 disemvowel(const char *str)
 {
-	int len = sizeof(str);
+	size_t len = strlen(str);
+
+	// create array to hold answer
+	size_t size = sizeof(char);
+	char *r_str;
+ 	if (size && len > SIZE_MAX / size) 
+		exit(-1);
+        if ((r_str = malloc(len * size)) == NULL)
+		exit(-1);
+
 	// Loop through str
+	int j=0;
 	for(int i = 0; i < len; i++) {
 		// Check each vowel
-		printf("%c", str[i]);
+		char c = tolower(str[i]);
+		if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+			continue;
+		} else {
+			r_str[j] = str[i];
+			j++;
+		}
 	}
-  return "Ths wbst s fr lsrs LL!";
+	// Add null terminator
+	r_str[j] = '\0';
+	return r_str;
 }
 
 int
 main(void) 
 {
-	printf("%i\n", strcmp(disemvowel("This website is for losers LOL!"), "Ths wbst s fr lsrs LL!") >= 0); 
-
+	char *r_str = disemvowel("This website is for losers LOL!");
+	printf("%s\n", r_str);
+	printf("%i\n", strcmp(r_str, "Ths wbst s fr lsrs LL!") >= 0); 
+	free(r_str);
 	return 0;
 }
